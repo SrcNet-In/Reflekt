98fe25ae2f9d56200e41b36e2354b8466f180460
Sayan Roy <sayanroy44617@gmail.com>
2025-05-21 10:49:11 +0530

    Initialize Spring Boot project with basic configuration and structure

---
+++ src/test/java/com/sayan/Code/Analyzer/CodeAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.Code.Analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
